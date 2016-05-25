e0bacd3a1c6da77d3c7203ae8562115c69241d15
AdrianoDayvson <adriano.dayvson@gmail.com>
2016-05-25 13:25:16 -0300

    fonts

---
+++ js/main.js
@@ -0,0 +1,57 @@
+function rotateCard(btn) {
+  var $card = $(btn).closest('.card-container');
+  console.log($card);
+  if ($card.hasClass('hover')) {
+    $card.removeClass('hover');
+  } else {
+    $card.addClass('hover');
+  }
+}
+
+function refreshCalcDisplay(sender){
+  var display = document.getElementById("calc-display");
+
+  if(sender.innerHTML=="=")
+  {
+    display.innerHTML = eval(display.innerHTML.replace("%","/100"));
+  }
+  else if (sender.innerHTML=="C")
+  {
+    display.innerHTML = display.innerHTML.slice(0,-1);
+  }
+  else if (sender.innerHTML=="AC")
+  {
+    display.innerHTML = "";
+  }
+  else if (sender.innerHTML=="()")
+  {//Need to be completed
+    /*
+    var lastChar = display.innerHTML[display.innerHTML.length-1];
+    if(lastChar=="."){
+      display.innerHTML = display.innerHTML + "0*(";
+    }else if(lastChar=="("){
+      display.innerHTML = display.innerHTML + "(";
+    }else if(lastChar=="+" || lastChar=="-" || lastChar=="*" || lastChar=="/"){
+      display.innerHTML = display.innerHTML + "(";
+    }else {
+      display.innerHTML = display.innerHTML + ")";
+    }
+    */
+  }
+  else
+  {
+    if(display.innerHTML.length>16)
+    {
+      if( $('#alertdiv').length==0)
+      {
+        $('#alert_placeholder')
+        .append('<div id="alertdiv" class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><span>You reach display\'s text limit! :(</span></div>');
+        setTimeout(function(){$("#alertdiv").remove();}, 5000);
+      }
+    }
+    else
+    {
+      display.innerHTML = display.innerHTML+sender.innerHTML;
+    }
+  }
+}
