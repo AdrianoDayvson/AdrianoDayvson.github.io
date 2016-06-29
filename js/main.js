b9f7c0b1500c5b3495ba6e8243fec6735d74a2d2
AdrianoDayvson <adriano.dayvson@gmail.com>
2016-06-28 21:15:58 -0300

    Dictionary - Finished
    Adicionado um dicionário técnico e mais duas imagens em Home

--- js/main.js
+++ js/main.js
@@ -108,3 +108,17 @@ $(".entrada" ).change(function(){
     drawChart3(parseFloat(sigx),parseFloat(sigy),parseFloat(tauxy),parseFloat(theta));
   }
 });
+
+
+var $rows = $('#table tr');
+$('#search').keyup(function() {
+
+    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+        reg = RegExp(val, 'i'),
+        text;
+
+    $rows.show().filter(function() {
+        text = $(this).text().replace(/\s+/g, ' ');
+        return !reg.test(text);
+    }).hide();
+});
